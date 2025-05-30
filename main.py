75353893d3503395f20d8582df9c4f05b01daeed
Tamilan003 <tamilan.test1@gmail.com>
2025-05-30 18:53:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -328,7 +328,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Or ["http://localhost:3000"] if React
+    allow_origins=["https://anand-vihar.onrender.com"],  # Or ["http://localhost:3000"] if React
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
