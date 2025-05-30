27b29608d441b94df33937703c3ed150774dd5b4
Tamilan003 <tamilan.test1@gmail.com>
2025-05-30 18:36:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ import shutil
 from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, validator
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or ["http://localhost:3000"] if React
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
